9d35e3b4825c3b5083dd46838f2b612528f86f94
Faberroni <raizal20ashari@apps.ipb.ac.id>
2022-11-06 15:25:43 +0700

    Changes to main app and UI

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import jsonify
-from flask import request
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
+    return render_template('index.html')
 
-    return('<h>Hello worlds</h>')
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
